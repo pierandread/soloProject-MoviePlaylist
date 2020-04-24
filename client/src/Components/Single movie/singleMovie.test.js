f3f71e90240e9060cdc30f939b625bcc7d146659
acandael <info@anthonycandaele.com>
2020-04-24 19:26:48 +0200

    finished testing

--- client/src/Components/Single movie/singleMovie.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import singleMovie from './SingleMovie';
-import SingleMovie from './SingleMovie';
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SingleMovie />, div);
-});
