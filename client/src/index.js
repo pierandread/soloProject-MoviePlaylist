cc78eb2e22e47ca86206f467de82c02d5d325ba0
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-04-28 17:23:47 +0200

    feat: add bootstrap react

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import allReducers from './Reducers';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 const store = createStore(
