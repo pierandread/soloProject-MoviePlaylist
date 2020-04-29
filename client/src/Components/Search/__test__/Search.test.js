bc04e4588e41d97bdb32d6de28b352011b97ed89
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-04-29 14:54:58 +0200

    feat: implemented tests for Search component

---
+++ client/src/Components/Search/__test__/Search.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, cleanup } from '@testing-library/react';
+import renderer, { act } from 'react-test-renderer';
+
+import Search from '../Search';
+
+describe ('Search component', () => {
+
+  afterEach(cleanup);
+
+  it ('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search />, div);
+  });
+
+  it ('should show search instructions when nothing has been typed', () => {
+    const component = render(<Search />);
+    expect(component.getByText(/Search a movie/i)).toBeInTheDocument();
+  });
+
+  it ('should NOT show search instructions when something has been typed', () => {
+    let component;
+    act(() => {
+      component = renderer.create(<Search />);
+    });
+    const instance = component.root;
+    const input = instance.findByType('input');
+    expect(instance.findAllByType('p').length).toBe(1);
+    input.props.onChange({target: { value: 'avengers' }});
+    expect(instance.findAllByType('p').length).toBe(0);
+  });
+
+});
